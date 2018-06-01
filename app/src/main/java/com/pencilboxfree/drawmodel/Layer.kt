8fdb06d567c7cc26cbc609d09178ab9b502d8f0b
Ruslan Iskhakov <deltasoftmailbox@gmail.com>
2018-06-01 23:41:03 +0500

    Work on Layer.kt

--- app/src/main/java/com/pencilboxfree/drawmodel/Layer.kt
+++ app/src/main/java/com/pencilboxfree/drawmodel/Layer.kt
@@ -1,10 +1,14 @@
 package com.pencilboxfree.drawmodel
 
 import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
 import com.pencilboxfree.interfaces.LayerInterface
 import com.pencilboxfree.interfaces.LayersManagerInterface
 
-class Layer constructor(layersManager: LayersManagerInterface, name: String, width: Int, height: Int, index: Int): LayerInterface{
+class Layer constructor(layersManager: LayersManagerInterface, name: String, width: Int, height: Int, index: Int, debug: Boolean=false): LayerInterface{
+
     private var _name: String = ""
     override var name: String
         get() {
@@ -43,6 +47,15 @@ class Layer constructor(layersManager: LayersManagerInterface, name: String, wid
             }
         }
 
+    private val _bitmap: Bitmap
+    override val bitmap: Bitmap
+        get() {
+            synchronized(this) {
+                return _bitmap
+            }
+        }
+
+
     companion object {
         fun create(
                 layersManager: LayersManager,
@@ -56,8 +69,6 @@ class Layer constructor(layersManager: LayersManagerInterface, name: String, wid
     val width: Int
     val height: Int
 
-    private val bitmap: Bitmap
-
     init {
         this.name = name
         this.layersManager = layersManager
@@ -67,13 +78,32 @@ class Layer constructor(layersManager: LayersManagerInterface, name: String, wid
 
         visible = true
 
-        bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
+        _bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
+
+        if (debug) {
+            val colors = intArrayOf(Color.RED, Color.BLUE, Color.GREEN, Color.YELLOW)
+            val paint = Paint()
+            paint.setColor(if (index < colors.size) colors[index] else colors[colors.size - 1])
+            paint.setStrokeWidth(10F)
+
+            val canvas = Canvas(bitmap)
+            canvas.drawText(index.toString(), 0F,0F,paint)
+        }
+
         storeLayer()
     }
 
     override fun recycle() {
-        if (!bitmap.isRecycled()) {
-            bitmap.recycle()
+        synchronized(this) {
+            if (!isRecycled()) {
+                bitmap.recycle()
+            }
+        }
+    }
+
+    override fun isRecycled(): Boolean {
+        synchronized(this) {
+            return bitmap.isRecycled()
         }
     }
 
--- app/src/main/java/com/pencilboxfree/interfaces/LayerInterface.kt
+++ app/src/main/java/com/pencilboxfree/interfaces/LayerInterface.kt
@@ -1,13 +1,16 @@
 package com.pencilboxfree.interfaces
 
+import android.graphics.Bitmap
 import com.pencilboxfree.drawmodel.LayerInfo
 
 interface LayerInterface {
     var name: String
     var index: Int
     var visible: Boolean
+    val bitmap: Bitmap
     fun storeLayer()
     fun restoreLayer()
     fun recycle()
     fun getInfo(): LayerInfo
+    fun isRecycled(): Boolean
 }
