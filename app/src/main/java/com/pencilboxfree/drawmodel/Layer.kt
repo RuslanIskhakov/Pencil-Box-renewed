c39680b1eb05110abc40b64c2d7e83c52f7f5ffb
Ruslan Iskhakov <deltasoftmailbox@gmail.com>
2018-06-05 16:05:01 +0500

    Synchronise getters and setters

--- app/src/main/java/com/pencilboxfree/drawmodel/Layer.kt
+++ app/src/main/java/com/pencilboxfree/drawmodel/Layer.kt
@@ -9,43 +9,15 @@ import com.pencilboxfree.interfaces.LayersManagerInterface
 
 class Layer constructor(layersManager: LayersManagerInterface, name: String, width: Int, height: Int, index: Int, debug: Boolean=false): LayerInterface{
 
-    private var _name: String = ""
-    override var name: String
-        get() {
-            synchronized(this) {
-                return _name
-            }
-        }
-        set(value) {
-            synchronized(this) {
-                _name = value
-            }
-        }
+    override var name: String = "" @Synchronized get @Synchronized set
 
-    private var _index: Int = 0
-    override var index: Int
-        get() {
-            synchronized(this) {
-                return _index
-            }
-        }
-        set(value) {
-            synchronized(this) {
-                _index = value
-            }
-        }
-    private var _visible = true
-    override var visible: Boolean
-        get() {
-            synchronized(this) {
-                return _visible
-            }
-        }
-        set(value) {
-            synchronized(this) {
-                _visible = value
-            }
-        }
+    override var index: Int = 0
+        @Synchronized get
+        @Synchronized set
+
+    override var visible: Boolean = true
+        @Synchronized get
+        @Synchronized set
 
     private val _bitmap: Bitmap
     override val bitmap: Bitmap
